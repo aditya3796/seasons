11a3eac8183029982802d1059e21bb7a45590a67
aditya <adityaagarwal371996@gmail.com>
2020-11-29 13:09:26 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import './SeasonDisplay.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    /*constructor(props) {
+        super(props);
+        //this is the only time we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };//initializing state, null for default value    
+    }*/
+    state = { lat: null, errorMessage: '' };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),//to update the state, only setState() function is used
+            err => this.setState({ errorMessage: err.message })
+        );
+    }    
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage} </div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+        );
+        /*return (
+        <div>
+            Latitude: {this.state.lat}<br />
+            Error: {this.state.errorMessage}
+        </div>);*/
+    }
+}
+
+ReactDOM.render(<App />,document.querySelector('#root'));
